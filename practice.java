a2c8487cd24de8190e4350136c5f3b66cebfead4
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2023-07-19 22:15:55 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,95 @@
+public class practice {
+    public static void main(String[] args) {
+
+
+//        Question 1
+//substraction
+        //
+//        int num1 ;
+//        int num2;
+//        num1= 40;
+//        num2= 30;
+//        System.out.println(num1-num2);
+
+
+        //  Question 2
+        // multiplication
+
+
+//            int num1 ;
+//      int num2;
+//      num1= 5;
+//      num2= 30;
+//    System.out.println(num1*num2);
+//        }
+//        }
+
+
+//    Question 3
+//        System.out.println("this is my ist day in intellij IDE and java is a oops+ language");
+//
+//    }
+//}
+
+
+//    Question 4
+
+//        for (int i = 10; i > 1; i--)
+//        System.out.println(i);
+//    }
+//}
+
+
+//Question 5
+
+//show even number
+
+//        int num1;
+//        num1 = 20;
+//        if (num1 % 2 == 0) {
+//            System.out.println("even");
+//        }
+//        else
+//        {
+//            System.out.println("odd");
+//        }
+//
+//    }
+//}
+
+
+//Question 6
+
+//show odd num1
+
+//        int num1;
+//        num1 = 30;
+//        if (num1 % 2 != 0) {
+//            System.out.println("Even");
+//        }
+//        else
+//        {
+//            System.out.println("odd");
+//
+//        }
+//    }
+//}
+//
+
+
+//Question 7
+//greatest of two
+
+        int num1;
+        int num2;
+        num1 = 30;
+        num2 = 1;
+        if (num1 > num2) {
+            System.out.println("num1 is greater");
+        } else {
+            System.out.println("num2 is greater");
+        }
+
+
+    }
+}

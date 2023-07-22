177477b1af340ee59e8bdf521f9a063031ad8ccd
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2023-07-22 20:13:12 +0530

    Add files via upload

---
+++ Home_practice_2.java
@@ -0,0 +1,174 @@
+import java.util.Scanner;
+public class Home_practice_2 {
+    public static void main(String[] args) {
+//        create calculator bty using  switch case
+//        int num1, num2, result = 0;
+//        Scanner pc = new Scanner(System.in);
+//        char op;
+//
+//        System.out.println("Enter any number");
+//        num1 = pc.nextInt();
+//
+//        System.out.println("Enter any number");
+//        num2 = pc.nextInt();
+//
+//        System.out.println("Enter operator");
+//        op = pc.next().charAt(0);
+//
+//
+//        switch (op) {
+//            case '+':
+//                result = num1 + num2;
+//                System.out.println(num1 + " + " + num2 + " = " + result);
+//                break;
+//
+//            case '-':
+//                result = num1 - num2;
+//                System.out.println(num1 + "-" + num2 + "=" + result);
+//                break;
+//
+//            case '*':
+//                result = num1 * num2;
+//                System.out.println(num1 + "*" + num2 + "=" + result);
+//                break;
+//
+//            case '/':
+//                result = num1 / num2;
+//                System.out.println(num1 + " /" + num2 + "=" + result);
+//                break;
+//
+//            default: {
+//                System.out.println("please enter valid number");
+//            }
+//
+//        }
+//
+//    }
+//}
+
+
+//question no 2
+
+
+        int num1 = 200;
+
+        switch (num1) {
+
+            case 20:
+                System.out.println("lesser");
+                break;
+
+            case 30:
+                System.out.println("smaller");
+                break;
+
+            case 100:
+                System.out.println("Normal");
+                break;
+            case 200:
+                System.out.println("equal");
+                break;
+            default:
+                System.out.println("please see this number ids greater & smaller");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

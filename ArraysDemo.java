973974a99b495533abac595f1131602079740155
Aasif hamid <102460661+azlaan23@users.noreply.github.com>
2023-07-26 15:15:31 +0530

    Add files via upload

---
+++ ArraysDemo.java
@@ -0,0 +1,19 @@
+public class ArraysDemo {
+    public static void main(String[] args) {
+        String names[] = {"amir", "adil", "rameez", "sahil", "muntazir", "junaid", "saim"};
+//        System.out.println(names[0]);
+
+//        for (int i=0;i<names.length;i++)
+//        {
+//            System.out.println(names[i]);
+//        }
+//
+//    }
+//}
+
+
+        for (int i = 6; i >= 0; i--) {
+            System.out.println(names[i]);
+        }
+    }
+}
